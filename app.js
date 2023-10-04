33600b1ac4406350512913800db1aa9e61fea0ac
moeezali <moeezali2375@gmail.com>
2023-10-05 03:06:02 +0500

    registeration is working on this

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+var passport = require("passport");
+var crypto = require("crypto");
+var routes = require("./routes");
+require("dotenv").config();
+const connection = connection.models.User;
 
-// Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+//! package to store sessions into mongodb-database
+const MongoStore = require("connect-mongo")(session);
 
 // Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
+require("./config/passport");
 
 /**
  * -------------- GENERAL SETUP ----------------
  */
 
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
 
 // Create the Express application
 var app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * -------------- SESSION SETUP ----------------
@@ -39,14 +38,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 /**
  * -------------- ROUTES ----------------
  */
 
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
-
 
 /**
  * -------------- SERVER ----------------
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,49 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const connection = require("./database");
+const error = require("mongoose/lib/error");
+const { validPassword } = require("../lib/passwordUtils");
 const User = connection.models.User;
 
-// TODO: passport.use();
+//! In case we have some custom name for username and password to look for in the db
+const customFields = {
+	usernameFields: "username",
+	passwordFeilds: "password",
+};
+
+const verifyCallback = (username, password, done) => {
+	User.findOne({ username: username })
+		.then((user) => {
+			if (!user) {
+				return done(null, false); //HELP No user found
+			}
+			//! hash the entered-password and check it with stored password
+			const isValid = validPassword(password, user.hash, user.salt);
+			if (isValid) {
+				return done(null, user); //HELP MATCH!
+			} else {
+				return done(null, false);
+			}
+		})
+		.catch((error) => {
+			done(error);
+		});
+};
+
+const Strategy = new LocalStrategy(verifyCallback, customFields);
+
+passport.use(Strategy);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((userId, done) => {
+	User.findById(userId)
+		.then((user) => {
+			done(null, user);
+		})
+		.catch((error) => {
+			done(error);
+		});
+});
